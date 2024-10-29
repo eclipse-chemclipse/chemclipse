6db6743fa82841ae97ad39cbbefab63d1cf07c9f
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-10-29 14:25:59 +0100

    Fix typo.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.filter/src/org/eclipse/chemclipse/chromatogram/wsd/filter/core/chromatogram/ChromatogramFilterWSDProcessSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.filter/src/org/eclipse/chemclipse/chromatogram/wsd/filter/core/chromatogram/ChromatogramFilterWSDProcessSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2023 Lablicate GmbH.
+ * Copyright (c) 2020, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -76,7 +76,7 @@ public class ChromatogramFilterWSDProcessSupplier implements IProcessTypeSupplie
 					messageConsumer.addMessages(ChromatogramFilterWSD.applyFilter(chromatogramSelectionWSD, supplier.getId(), monitor));
 				}
 			} else {
-				messageConsumer.addWarnMessage(getName(), "Only WSD chromatogram supported, skipp processing");
+				messageConsumer.addWarnMessage(getName(), "Only WSD chromatogram supported, skip processing");
 			}
 			return chromatogramSelection;
 		}
--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.identifier/src/org/eclipse/chemclipse/chromatogram/wsd/identifier/peak/PeakIdentifierWSDProcessTypeSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.identifier/src/org/eclipse/chemclipse/chromatogram/wsd/identifier/peak/PeakIdentifierWSDProcessTypeSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2023 Lablicate GmbH.
+ * Copyright (c) 2019, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -76,7 +76,7 @@ public class PeakIdentifierWSDProcessTypeSupplier implements IProcessTypeSupplie
 					messageConsumer.addMessages(PeakIdentifierWSD.identify(chromatogramSelectionWSD, supplier.getId(), monitor));
 				}
 			} else {
-				messageConsumer.addWarnMessage(getName(), "Only WSD chromatogram supported, skipp processing");
+				messageConsumer.addWarnMessage(getName(), "Only WSD chromatogram supported, skip processing");
 			}
 			return chromatogramSelection;
 		}
