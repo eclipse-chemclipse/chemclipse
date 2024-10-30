6d749cc46aa01e525a57c61fd9c93cdad12b27fb
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-10-30 14:28:13 +0100

    Use a better description.

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.filter/src/org/eclipse/chemclipse/xxd/filter/peaks/DeleteIntegrationsFilter.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.filter/src/org/eclipse/chemclipse/xxd/filter/peaks/DeleteIntegrationsFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2022 Lablicate GmbH.
+ * Copyright (c) 2020, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -35,7 +35,7 @@ public class DeleteIntegrationsFilter extends AbstractPeakFilter<DeleteIntegrati
 	@Override
 	public String getDescription() {
 
-		return "Filter Peak Integrations";
+		return "Reset calculated area/height.";
 	}
 
 	@Override
@@ -51,7 +51,6 @@ public class DeleteIntegrationsFilter extends AbstractPeakFilter<DeleteIntegrati
 		if(configuration == null) {
 			configuration = createConfiguration(peaks);
 		}
-		//
 		if(configuration.isDeleteIntegrations()) {
 			SubMonitor subMonitor = SubMonitor.convert(context.getProgressMonitor(), peaks.size());
 			for(IPeak peak : peaks) {
