395279a91ab30a3c8258f083ee79b25ece102221
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-11-04 20:30:33 +0100

    Fix Help - Search/Context menu entry not opening.

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationActionBarAdvisor.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/ApplicationActionBarAdvisor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2023 Lablicate GmbH.
+ * Copyright (c) 2008, 2024 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -11,10 +11,19 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.rcp.app.ui;
 
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	@Override
+	protected void makeActions(IWorkbenchWindow window) {
+
+		register(ActionFactory.HELP_SEARCH.create(window));
+		register(ActionFactory.DYNAMIC_HELP.create(window));
+	}
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 
