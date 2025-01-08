334db11623282b9c952d4393c3068f9dd48aa523
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-01-08 20:47:18 +0100

    Modularize mass spectrum reader.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/internal/io/MassSpectrumReaderVersion105.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.converter.supplier.mzdata/src/org/eclipse/chemclipse/msd/converter/supplier/mzdata/internal/io/MassSpectrumReaderVersion105.java
@@ -21,7 +21,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.msd.converter.io.AbstractMassSpectraReader;
 import org.eclipse.chemclipse.msd.converter.io.IMassSpectraReader;
+import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.AdminType;
 import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.CvParamType;
+import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.Description;
 import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.MzData;
 import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.ObjectFactory;
 import org.eclipse.chemclipse.msd.converter.supplier.mzdata.internal.v105.model.ParamType;
@@ -65,44 +67,10 @@ public class MassSpectrumReaderVersion105 extends AbstractMassSpectraReader impl
 			MzData mzData = (MzData)unmarshaller.unmarshal(nodeList.item(0));
 			//
 			massSpectrum = new StandaloneMassSpectrum();
-			massSpectrum.setSampleName(mzData.getDescription().getAdmin().getSampleName());
-			for(PersonType contact : mzData.getDescription().getAdmin().getContact()) {
-				String contactDetails = "";
-				if(contact.getContactInfo() != null) {
-					contactDetails = String.join(", ", contact.getName(), contact.getInstitution(), contact.getContactInfo());
-				} else {
-					contactDetails = String.join(", ", contact.getName(), contact.getInstitution());
-				}
-				if(massSpectrum.getOperator() == null || massSpectrum.getOperator().isEmpty()) {
-					massSpectrum.setOperator(contactDetails);
-				} else {
-					massSpectrum.setOperator(String.join(", ", massSpectrum.getOperator(), contactDetails));
-				}
-			}
-			massSpectrum.setInstrument(mzData.getDescription().getInstrument().getInstrumentName());
 			massSpectrum.setFile(file);
 			massSpectrum.setIdentifier(file.getName());
-			ParamType processingMethod = mzData.getDescription().getDataProcessing().getProcessingMethod();
-			if(processingMethod != null) {
-				for(Object object : processingMethod.getCvParamOrUserParam()) {
-					if(object instanceof CvParamType cvParamType) {
-						if(cvParamType.getName().equals("peakProcessing")) {
-							if(cvParamType.getValue().equals("centroided")) {
-								massSpectrum.setMassSpectrumType(MassSpectrumType.CENTROID);
-							} else {
-								massSpectrum.setMassSpectrumType(MassSpectrumType.PROFILE);
-							}
-						}
-					}
-				}
-			}
-			Spectrum spectrum = mzData.getSpectrumList().getSpectrum().get(0);
-			double[] mzs = ReaderVersion105.parseData(spectrum.getMzArrayBinary().getData());
-			double[] intensities = ReaderVersion105.parseData(spectrum.getIntenArrayBinary().getData());
-			int length = Math.min(mzs.length, intensities.length);
-			for(int i = 0; i < length; i++) {
-				massSpectrum.addIon(new VendorIon(mzs[i], (float)intensities[i]), false);
-			}
+			readDescription(mzData, massSpectrum);
+			readIons(mzData, massSpectrum);
 		} catch(SAXException e) {
 			logger.warn(e);
 		} catch(JAXBException e) {
@@ -115,5 +83,67 @@ public class MassSpectrumReaderVersion105 extends AbstractMassSpectraReader impl
 		massSpectra.setName(file.getName());
 		massSpectra.addMassSpectrum(massSpectrum);
 		return massSpectra;
+	}
+
+	private void readDescription(MzData mzData, IStandaloneMassSpectrum massSpectrum) {
+
+		Description description = mzData.getDescription();
+		if(description != null) {
+			readAdmin(description, massSpectrum);
+		}
+		massSpectrum.setInstrument(description.getInstrument().getInstrumentName());
+		readProcessingMethod(description, massSpectrum);
+	}
+
+	private void readProcessingMethod(Description description, IStandaloneMassSpectrum massSpectrum) {
+
+		ParamType processingMethod = description.getDataProcessing().getProcessingMethod();
+		if(processingMethod == null) {
+			return;
+		}
+		for(Object object : processingMethod.getCvParamOrUserParam()) {
+			if(object instanceof CvParamType cvParamType) {
+				if(cvParamType.getName().equals("peakProcessing")) {
+					if(cvParamType.getValue().equals("centroided")) {
+						massSpectrum.setMassSpectrumType(MassSpectrumType.CENTROID);
+					} else {
+						massSpectrum.setMassSpectrumType(MassSpectrumType.PROFILE);
+					}
+				}
+			}
+		}
+	}
+
+	private void readAdmin(Description description, IStandaloneMassSpectrum massSpectrum) {
+
+		AdminType admin = description.getAdmin();
+		if(admin == null) {
+			return;
+		}
+		massSpectrum.setSampleName(admin.getSampleName());
+		for(PersonType contact : admin.getContact()) {
+			String contactDetails = "";
+			if(contact.getContactInfo() != null) {
+				contactDetails = String.join(", ", contact.getName(), contact.getInstitution(), contact.getContactInfo());
+			} else {
+				contactDetails = String.join(", ", contact.getName(), contact.getInstitution());
+			}
+			if(massSpectrum.getOperator() == null || massSpectrum.getOperator().isEmpty()) {
+				massSpectrum.setOperator(contactDetails);
+			} else {
+				massSpectrum.setOperator(String.join(", ", massSpectrum.getOperator(), contactDetails));
+			}
+		}
+	}
+
+	private void readIons(MzData mzData, IStandaloneMassSpectrum massSpectrum) {
+
+		Spectrum spectrum = mzData.getSpectrumList().getSpectrum().get(0);
+		double[] mzs = ReaderVersion105.parseData(spectrum.getMzArrayBinary().getData());
+		double[] intensities = ReaderVersion105.parseData(spectrum.getIntenArrayBinary().getData());
+		int length = Math.min(mzs.length, intensities.length);
+		for(int i = 0; i < length; i++) {
+			massSpectrum.addIon(new VendorIon(mzs[i], (float)intensities[i]), false);
+		}
 	}
 }
