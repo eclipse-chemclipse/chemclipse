fadba386b3bca5c78233a5ef258826198dac92a0
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-01-15 10:40:16 +0100

    Remove unused MSD overlay offset settings.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/preferences/PreferenceSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/preferences/PreferenceSupplier.java
@@ -17,14 +17,6 @@ import org.eclipse.chemclipse.ux.extension.msd.ui.Activator;
 
 public class PreferenceSupplier extends AbstractPreferenceSupplier implements IPreferenceSupplier {
 
-	public static final int MIN_X_OFFSET = 0; // = 0.0 minutes
-	public static final int MAX_X_OFFSET = 6000000; // = 100.0 minutes;
-	//
-	public static final String P_OVERLAY_X_OFFSET = "overlayXOffset";
-	public static final int DEF_OVERLAY_X_OFFSET = 0;
-	public static final String P_OVERLAY_Y_OFFSET = "overlayYOffset";
-	public static final int DEF_OVERLAY_Y_OFFSET = 0;
-	//
 	public static final String P_MAGNIFICATION_FACTOR = "magnificationFactor";
 	public static final int DEF_MAGNIFICATION_FACTOR = 1;
 	public static final int DEF_MAGNIFICATION_FACTOR_MIN = 1;
@@ -47,30 +39,8 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	@Override
 	public void initializeDefaults() {
 
-		putDefault(P_OVERLAY_X_OFFSET, DEF_OVERLAY_X_OFFSET);
-		putDefault(P_OVERLAY_Y_OFFSET, DEF_OVERLAY_Y_OFFSET);
 		putDefault(PreferenceSupplier.P_MAGNIFICATION_FACTOR, PreferenceSupplier.DEF_MAGNIFICATION_FACTOR);
 		putDefault(PreferenceSupplier.P_USE_PROFILE_MASS_SPECTRUM_VIEW, PreferenceSupplier.DEF_USE_PROFILE_MASS_SPECTRUM_VIEW);
-	}
-
-	/**
-	 * Returns the x offset value.
-	 * 
-	 * @return int
-	 */
-	public static int getOverlayXOffset() {
-
-		return INSTANCE().getInteger(P_OVERLAY_X_OFFSET);
-	}
-
-	/**
-	 * Returns the y offset value.
-	 * 
-	 * @return int
-	 */
-	public static int getOverlayYOffset() {
-
-		return INSTANCE().getInteger(P_OVERLAY_Y_OFFSET);
 	}
 
 	public static boolean useProfileMassSpectrumView() {
