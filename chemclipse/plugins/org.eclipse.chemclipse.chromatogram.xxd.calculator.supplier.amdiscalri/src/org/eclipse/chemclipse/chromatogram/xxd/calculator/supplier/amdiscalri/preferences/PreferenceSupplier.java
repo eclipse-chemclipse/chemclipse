8f0691994ac0b36a3e7e14b839849ee0c737a550
Philip Wenig <philip.wenig@openchrom.net>
2025-01-22 11:07:42 +0100

    WiP #2044 - group internally used list path settings

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/preferences/PreferenceSupplier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.calculator.supplier.amdiscalri/src/org/eclipse/chemclipse/chromatogram/xxd/calculator/supplier/amdiscalri/preferences/PreferenceSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2024 Lablicate GmbH.
+ * Copyright (c) 2014, 2025 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -41,12 +41,6 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	public static final boolean DEF_USE_DEFAULT_COLUMN = true;
 	public static final String P_PROCESS_REFERENCED_CHROMATOGRAMS = "processReferencedChromatograms";
 	public static final boolean DEF_PROCESS_REFERENCED_CHROMATOGRAMS = true;
-	public static final String P_FILTER_PATH_INDEX_FILES = "filterPathIndexFiles";
-	public static final String DEF_FILTER_PATH_INDEX_FILES = "";
-	public static final String P_FILTER_PATH_MODELS_MSD = "filterPathModelsMSD";
-	public static final String DEF_FILTER_PATH_MODELS_MSD = "";
-	public static final String P_FILTER_PATH_MODELS_CSD = "filterPathModelsCSD";
-	public static final String DEF_FILTER_PATH_MODELS_CSD = "";
 	public static final String P_OPEN_REPORT_AFTER_PROCESSING = "openReportAfterProcessing";
 	public static final boolean DEF_OPEN_REPORT_AFTER_PROCESSING = true;
 	/*
@@ -64,13 +58,6 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	public static final float DEF_MIN_REVERSE_MATCH_FACTOR = 70.0f;
 	public static final float MIN_MIN_REVERSE_MATCH_FACTOR = 0.0f;
 	public static final float MAX_MIN_REVERSE_MATCH_FACTOR = 100.0f;
-	//
-	public static final String P_LIST_PATH_IMPORT_FILE = "listPathImportCalibrationFile";
-	public static final String DEF_LIST_PATH_IMPORT_FILE = "";
-	public static final String P_LIST_PATH_IMPORT_TEMPLATE = "listPathImportTemplate";
-	public static final String DEF_LIST_PATH_IMPORT_TEMPLATE = "";
-	public static final String P_LIST_PATH_EXPORT_TEMPLATE = "listPathExportTemplate";
-	public static final String DEF_LIST_PATH_EXPORT_TEMPLATE = "";
 	/*
 	 * Cal Export
 	 */
@@ -79,8 +66,20 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	public static final String P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES = "calibrationExportDeriveMissingIndices";
 	public static final boolean DEF_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES = true;
 	/*
-	 * General
+	 * Locations (handled internally)
 	 */
+	public static final String P_FILTER_PATH_INDEX_FILES = "filterPathIndexFiles";
+	public static final String DEF_FILTER_PATH_INDEX_FILES = "";
+	public static final String P_FILTER_PATH_MODELS_MSD = "filterPathModelsMSD";
+	public static final String DEF_FILTER_PATH_MODELS_MSD = "";
+	public static final String P_FILTER_PATH_MODELS_CSD = "filterPathModelsCSD";
+	public static final String DEF_FILTER_PATH_MODELS_CSD = "";
+	public static final String P_LIST_PATH_IMPORT_FILE = "listPathImportCalibrationFile";
+	public static final String DEF_LIST_PATH_IMPORT_FILE = "";
+	public static final String P_LIST_PATH_IMPORT_TEMPLATE = "listPathImportTemplate";
+	public static final String DEF_LIST_PATH_IMPORT_TEMPLATE = "";
+	public static final String P_LIST_PATH_EXPORT_TEMPLATE = "listPathExportTemplate";
+	public static final String DEF_LIST_PATH_EXPORT_TEMPLATE = "";
 	public static final String P_LIST_PATH_IMPORT = "listPathImport";
 	public static final String DEF_LIST_PATH_IMPORT = "";
 	public static final String P_LIST_PATH_EXPORT = "listPathExport";
@@ -104,20 +103,21 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 		putDefault(P_CALCULATOR_STRATEGY, DEF_CALCULATOR_STRATEGY);
 		putDefault(P_USE_DEFAULT_COLUMN, Boolean.toString(DEF_USE_DEFAULT_COLUMN));
 		putDefault(P_PROCESS_REFERENCED_CHROMATOGRAMS, Boolean.toString(DEF_PROCESS_REFERENCED_CHROMATOGRAMS));
-		putDefault(P_FILTER_PATH_INDEX_FILES, DEF_FILTER_PATH_INDEX_FILES);
-		putDefault(P_FILTER_PATH_MODELS_MSD, DEF_FILTER_PATH_MODELS_MSD);
-		putDefault(P_FILTER_PATH_MODELS_CSD, DEF_FILTER_PATH_MODELS_CSD);
 		putDefault(P_NUMBER_OF_TARGETS, Integer.toString(DEF_NUMBER_OF_TARGETS));
 		putDefault(P_MIN_MATCH_FACTOR, Float.toString(DEF_MIN_MATCH_FACTOR));
 		putDefault(P_MIN_REVERSE_MATCH_FACTOR, Float.toString(DEF_MIN_REVERSE_MATCH_FACTOR));
+		putDefault(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, Boolean.toString(DEF_CALIBRATION_EXPORT_USE_CURATED_NAMES));
+		putDefault(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, Boolean.toString(DEF_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES));
+		putDefault(P_OPEN_REPORT_AFTER_PROCESSING, Boolean.toString(DEF_OPEN_REPORT_AFTER_PROCESSING));
+		//
+		putDefault(P_FILTER_PATH_INDEX_FILES, DEF_FILTER_PATH_INDEX_FILES);
+		putDefault(P_FILTER_PATH_MODELS_MSD, DEF_FILTER_PATH_MODELS_MSD);
+		putDefault(P_FILTER_PATH_MODELS_CSD, DEF_FILTER_PATH_MODELS_CSD);
 		putDefault(P_LIST_PATH_IMPORT_FILE, DEF_LIST_PATH_IMPORT_FILE);
 		putDefault(P_LIST_PATH_IMPORT_TEMPLATE, DEF_LIST_PATH_IMPORT_TEMPLATE);
 		putDefault(P_LIST_PATH_EXPORT_TEMPLATE, DEF_LIST_PATH_EXPORT_TEMPLATE);
-		putDefault(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, Boolean.toString(DEF_CALIBRATION_EXPORT_USE_CURATED_NAMES));
-		putDefault(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, Boolean.toString(DEF_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES));
 		putDefault(P_LIST_PATH_IMPORT, DEF_LIST_PATH_IMPORT);
 		putDefault(P_LIST_PATH_EXPORT, DEF_LIST_PATH_EXPORT);
-		putDefault(P_OPEN_REPORT_AFTER_PROCESSING, Boolean.toString(DEF_OPEN_REPORT_AFTER_PROCESSING));
 	}
 
 	public static PeakIdentifierAlkaneSettings getPeakIdentifierAlkaneSettings() {
@@ -169,6 +169,61 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 		INSTANCE().put(P_RETENTION_INDEX_FILES, fileListUtil.createList(items));
 	}
 
+	public static int getNumberOfTargets() {
+
+		return INSTANCE().getInteger(P_NUMBER_OF_TARGETS, DEF_NUMBER_OF_TARGETS);
+	}
+
+	public static float getMinMatchFactor() {
+
+		return INSTANCE().getFloat(P_MIN_MATCH_FACTOR, DEF_MIN_MATCH_FACTOR);
+	}
+
+	public static float getMinReverseMatchFactor() {
+
+		return INSTANCE().getFloat(P_MIN_REVERSE_MATCH_FACTOR, DEF_MIN_REVERSE_MATCH_FACTOR);
+	}
+
+	public static boolean isCalibrationExportUseCuratedNames() {
+
+		return INSTANCE().getBoolean(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, DEF_CALIBRATION_EXPORT_USE_CURATED_NAMES);
+	}
+
+	public static void setCalibrationExportUseCuratedNames(boolean value) {
+
+		INSTANCE().putBoolean(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, value);
+	}
+
+	public static boolean isCalibrationExportDeriveMissingIndices() {
+
+		return INSTANCE().getBoolean(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, DEF_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES);
+	}
+
+	public static void setCalibrationExportDeriveMissingIndices(boolean value) {
+
+		INSTANCE().putBoolean(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, value);
+	}
+
+	public static boolean isOpenReportAfterProcessing() {
+
+		return INSTANCE().getBoolean(P_OPEN_REPORT_AFTER_PROCESSING, DEF_OPEN_REPORT_AFTER_PROCESSING);
+	}
+
+	private static CalculatorStrategy getCalculatorStrategy() {
+
+		return CalculatorStrategy.valueOf(INSTANCE().get(P_CALCULATOR_STRATEGY, DEF_CALCULATOR_STRATEGY));
+	}
+
+	private static boolean isUseDefaultColumn() {
+
+		return INSTANCE().getBoolean(P_USE_DEFAULT_COLUMN, DEF_USE_DEFAULT_COLUMN);
+	}
+
+	private static boolean isProcessReferencedChromatograms() {
+
+		return INSTANCE().getBoolean(P_PROCESS_REFERENCED_CHROMATOGRAMS, DEF_PROCESS_REFERENCED_CHROMATOGRAMS);
+	}
+
 	public static String getFilterPathIndexFiles() {
 
 		return INSTANCE().get(P_FILTER_PATH_INDEX_FILES, DEF_FILTER_PATH_INDEX_FILES);
@@ -197,21 +252,6 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	public static void setFilterPathModelsCSD(String filterPath) {
 
 		INSTANCE().put(P_FILTER_PATH_MODELS_CSD, filterPath);
-	}
-
-	public static int getNumberOfTargets() {
-
-		return INSTANCE().getInteger(P_NUMBER_OF_TARGETS, DEF_NUMBER_OF_TARGETS);
-	}
-
-	public static float getMinMatchFactor() {
-
-		return INSTANCE().getFloat(P_MIN_MATCH_FACTOR, DEF_MIN_MATCH_FACTOR);
-	}
-
-	public static float getMinReverseMatchFactor() {
-
-		return INSTANCE().getFloat(P_MIN_REVERSE_MATCH_FACTOR, DEF_MIN_REVERSE_MATCH_FACTOR);
 	}
 
 	public static String getListPathImportFile() {
@@ -244,26 +284,6 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 		INSTANCE().put(P_LIST_PATH_EXPORT_TEMPLATE, filterPath);
 	}
 
-	public static boolean isCalibrationExportUseCuratedNames() {
-
-		return INSTANCE().getBoolean(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, DEF_CALIBRATION_EXPORT_USE_CURATED_NAMES);
-	}
-
-	public static void setCalibrationExportUseCuratedNames(boolean value) {
-
-		INSTANCE().putBoolean(P_CALIBRATION_EXPORT_USE_CURATED_NAMES, value);
-	}
-
-	public static boolean isCalibrationExportDeriveMissingIndices() {
-
-		return INSTANCE().getBoolean(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, DEF_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES);
-	}
-
-	public static void setCalibrationExportDeriveMissingIndices(boolean value) {
-
-		INSTANCE().putBoolean(P_CALIBRATION_EXPORT_DERIVE_MISSING_INDICES, value);
-	}
-
 	public static String getListPathImport() {
 
 		return INSTANCE().get(P_LIST_PATH_IMPORT, DEF_LIST_PATH_IMPORT);
@@ -282,25 +302,5 @@ public class PreferenceSupplier extends AbstractPreferenceSupplier implements IP
 	public static void setListPathExport(String filterPath) {
 
 		INSTANCE().put(P_LIST_PATH_EXPORT, filterPath);
-	}
-
-	public static boolean isOpenReportAfterProcessing() {
-
-		return INSTANCE().getBoolean(P_OPEN_REPORT_AFTER_PROCESSING, DEF_OPEN_REPORT_AFTER_PROCESSING);
-	}
-
-	private static CalculatorStrategy getCalculatorStrategy() {
-
-		return CalculatorStrategy.valueOf(INSTANCE().get(P_CALCULATOR_STRATEGY, DEF_CALCULATOR_STRATEGY));
-	}
-
-	private static boolean isUseDefaultColumn() {
-
-		return INSTANCE().getBoolean(P_USE_DEFAULT_COLUMN, DEF_USE_DEFAULT_COLUMN);
-	}
-
-	private static boolean isProcessReferencedChromatograms() {
-
-		return INSTANCE().getBoolean(P_PROCESS_REFERENCED_CHROMATOGRAMS, DEF_PROCESS_REFERENCED_CHROMATOGRAMS);
 	}
 }
