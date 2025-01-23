4c8c9f1ccd789132efd94adcea46b0e38c81cd71
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-01-23 10:21:52 +0100

    Code cleanup

--- chemclipse/plugins/org.eclipse.chemclipse.wsd.converter.supplier.mzml/src/org/eclipse/chemclipse/wsd/converter/supplier/mzml/model/VendorChromatogram.java
+++ chemclipse/plugins/org.eclipse.chemclipse.wsd.converter.supplier.mzml/src/org/eclipse/chemclipse/wsd/converter/supplier/mzml/model/VendorChromatogram.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2024 Lablicate GmbH.
+ * Copyright (c) 2021, 2025 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -7,14 +7,15 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Philip Wenig - initial API and implementation
+ * Matthias Mailänder - initial API and implementation
  *******************************************************************************/
 package org.eclipse.chemclipse.wsd.converter.supplier.mzml.model;
 
 import org.eclipse.chemclipse.wsd.model.core.AbstractChromatogramWSD;
 
-@SuppressWarnings("serial")
 public class VendorChromatogram extends AbstractChromatogramWSD implements IVendorChromatogram {
+
+	private static final long serialVersionUID = 2874207336794926052L;
 
 	@Override
 	public String getName() {
