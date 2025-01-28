5f565f914fa10b8965a3fcd42b98104493fc4177
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-01-28 14:02:49 +0100

    Fall back to standard settings to avoid a crash.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.report.supplier.pdf/src/org/eclipse/chemclipse/chromatogram/xxd/report/supplier/pdf/core/ChromatogramReportGenerator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.report.supplier.pdf/src/org/eclipse/chemclipse/chromatogram/xxd/report/supplier/pdf/core/ChromatogramReportGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Lablicate GmbH.
+ * Copyright (c) 2024, 2025 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -48,7 +48,7 @@ public class ChromatogramReportGenerator extends AbstractChromatogramReportGener
 	@Override
 	public IProcessingInfo<?> generate(File file, boolean append, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
-		throw new UnsupportedOperationException();
+		return generate(file, append, chromatogram, new ReportSettings(), monitor);
 	}
 
 	@Override
