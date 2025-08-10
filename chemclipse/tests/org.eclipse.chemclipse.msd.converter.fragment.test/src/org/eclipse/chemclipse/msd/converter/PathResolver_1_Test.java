72c0c4ed1a7d1b257b2e6af03f01d3faaab6e081
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-08-10 14:22:57 +0200

    Fix path resolver test for CI environment.

--- chemclipse/tests/org.eclipse.chemclipse.msd.converter.fragment.test/src/org/eclipse/chemclipse/msd/converter/PathResolver_1_Test.java
+++ chemclipse/tests/org.eclipse.chemclipse.msd.converter.fragment.test/src/org/eclipse/chemclipse/msd/converter/PathResolver_1_Test.java
@@ -12,47 +12,38 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.msd.converter;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
-import org.eclipse.chemclipse.msd.converter.chromatogram.ChromatogramConverterMSD;
-import org.junit.Ignore;
-import org.osgi.framework.FrameworkUtil;
+import org.eclipse.core.runtime.FileLocator;
+import org.junit.Test;
 
-import junit.framework.TestCase;
+public class PathResolver_1_Test {
 
-@Ignore("Runs fine in the IDE, fails via Maven.")
-public class PathResolver_1_Test extends TestCase {
+	@Test
+	public void testGetAbsolutePath_1() throws IOException, URISyntaxException {
 
-	public void testGetAbsolutePath_1() {
-
-		try {
-			String location = FrameworkUtil.getBundle(ChromatogramConverterMSD.class).getLocation();
-			location = location.replace("reference:file:", "");
-			if(location.endsWith("/")) {
-				location = location.substring(0, location.length() - 1);
+		URL url = getClass().getClassLoader().getResource(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_EMPTY);
+		String test = PathResolver.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_EMPTY);
+		String path = new File(FileLocator.toFileURL(url).toURI()).getAbsolutePath();
+		// Handle Windows systems in a different way than unix/linux :-).
+		String osName = System.getProperty("os.name").toLowerCase();
+		if(osName.contains("windows")) {
+			if(test.startsWith("/")) {
+				test = test.substring(1);
 			}
-			location = location.concat(".fragment.test");
-			File file = new File(location);
-			String path = file.getAbsolutePath() + "/testData/files/import/EMPTY.D/DATA.MS";
-			String test = PathResolver.getAbsolutePath(TestPathHelper.TESTFILE_IMPORT_CHROMATOGRAM_EMPTY);
-			// Handle Windows systems in a different way than unix/linux :-).
-			String osName = System.getProperty("os.name").toLowerCase();
-			if(osName.contains("windows")) {
-				if(test.startsWith("/")) {
-					test = test.substring(1);
-				}
-				test = test.replace('\\', '/');
-				path = path.replace('\\', '/');
-			}
-			/*
-			 * Tests folder, but path resolver is located in plugins.
-			 * .../git/chemclipse/chemclipse/tests/org.eclipse.chemclipse.msd.converter.fragment.test/testData/files/import/EMPTY.D/DATA.MS
-			 */
-			path = path.replace("plugins", "tests");
-			assertEquals(path, test);
-		} catch(IOException e) {
-			assertTrue("IOException", false);
+			test = test.replace('\\', '/');
+			path = path.replace('\\', '/');
 		}
+		/*
+		 * Tests folder, but path resolver is located in plugins.
+		 * .../git/chemclipse/chemclipse/tests/org.eclipse.chemclipse.msd.converter.fragment.test/testData/files/import/EMPTY.D/DATA.MS
+		 */
+		path = path.replace("plugins", "tests");
+		assertEquals(path, test);
 	}
 }
