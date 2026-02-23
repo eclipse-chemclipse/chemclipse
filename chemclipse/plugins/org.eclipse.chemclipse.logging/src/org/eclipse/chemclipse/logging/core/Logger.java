3814385ee1ac9c1cf6e2ac7c8978dcc2fee2c28b
Александър Куртаков <akurtakov@gmail.com>
2026-02-23 21:57:12 +0200

    Extend Logger to auto fill the caller class

--- chemclipse/plugins/org.eclipse.chemclipse.logging/src/org/eclipse/chemclipse/logging/core/Logger.java
+++ chemclipse/plugins/org.eclipse.chemclipse.logging/src/org/eclipse/chemclipse/logging/core/Logger.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2025 Lablicate GmbH.
+ * Copyright (c) 2008, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,10 +13,9 @@
 package org.eclipse.chemclipse.logging.core;
 
 public class Logger extends Category {
+	
+	private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
-	/**
-	 * @param clazz
-	 */
 	protected Logger(Class<?> clazz) {
 
 		super(clazz);
@@ -24,12 +23,17 @@ public class Logger extends Category {
 
 	/**
 	 * Returns the logger.
-	 * 
-	 * @param clazz
-	 * @return Logger
 	 */
 	public static Logger getLogger(Class<?> clazz) {
 
 		return new Logger(clazz);
+	}
+	
+	/**
+	 * Returns the logger for the caller class.
+	 */
+	public static Logger getLogger() {
+
+		return new Logger(STACK_WALKER.getCallerClass());
 	}
 }
