35c55721e5c0707c514d7c6338bae7c8da1d248d
Александър Куртаков <akurtakov@gmail.com>
2026-03-05 14:44:58 +0200

    Inject IWorkbenchWindow in ResetPerspectiveHandler
    Instead of statically access it through PlatformUI

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/handlers/ResetPerspectiveHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.app.ui/src/org/eclipse/chemclipse/rcp/app/ui/handlers/ResetPerspectiveHandler.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2025 Lablicate GmbH.
+ * Copyright (c) 2021, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  * Matthias Mailänder - initial API and implementation
  *******************************************************************************/
@@ -17,14 +17,14 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.IWorkbenchWindow;
 
 public class ResetPerspectiveHandler {
 
 	@Execute
-	public void execute(MWindow window, IEventBroker eventBroker) {
+	public void execute(MWindow window, IEventBroker eventBroker, IWorkbenchWindow workbenchWindow) {
 
-		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IWorkbenchPage page = workbenchWindow.getActivePage();
 		page.resetPerspective();
 		eventBroker.post(IChemClipseEvents.TOPIC_APPLICATION_RESET_PERSPECTIVE, page.getPerspective().getLabel());
 	}
