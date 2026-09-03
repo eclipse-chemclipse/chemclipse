6a19fcd0f0a08d01cd41ea3ccb5570314d68f29d
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-09-03 19:20:18 +0200

    Add an icon for the baseline subtract filter.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.baselinesubtract.ui/src/org/eclipse/chemclipse/chromatogram/xxd/filter/supplier/baselinesubtract/ui/icon/BaselineSubtractMenuIcon.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.filter.supplier.baselinesubtract.ui.icon;
+
+import org.eclipse.chemclipse.rcp.ui.icons.core.ApplicationImageFactory;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImageProvider;
+import org.eclipse.chemclipse.xxd.process.ui.menu.IMenuIcon;
+import org.eclipse.swt.graphics.Image;
+
+public class BaselineSubtractMenuIcon implements IMenuIcon {
+
+	@Override
+	public Image getImage() {
+
+		return ApplicationImageFactory.getInstance().getImage(IApplicationImage.IMAGE_BASELINE, IApplicationImageProvider.SIZE_16x16);
+	}
+}
