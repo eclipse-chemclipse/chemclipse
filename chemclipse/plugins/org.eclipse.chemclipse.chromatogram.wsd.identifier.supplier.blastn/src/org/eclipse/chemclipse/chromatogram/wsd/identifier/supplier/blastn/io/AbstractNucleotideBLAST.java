c7d7297af28d102de412a528920b9002eef5188d
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-09-10 15:19:13 +0200

    Store scientific name from taxonomy database as synonym
    and warn when the result is empty.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.identifier.supplier.blastn/src/org/eclipse/chemclipse/chromatogram/wsd/identifier/supplier/blastn/io/AbstractNucleotideBLAST.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.wsd.identifier.supplier.blastn/src/org/eclipse/chemclipse/chromatogram/wsd/identifier/supplier/blastn/io/AbstractNucleotideBLAST.java
@@ -55,6 +55,7 @@ public abstract class AbstractNucleotideBLAST {
 			libraryInformation.setGenBankAccesion(description.getAccession());
 			libraryInformation.setReferenceIdentifier(description.getId());
 			libraryInformation.setTaxonomyIdentifierNCBI(description.getTaxid().intValue());
+			libraryInformation.getSynonyms().add(description.getSciname());
 			for(Hsp hsp : hit.getHsps().getHsp()) {
 				IdentificationTarget identificationTarget = new IdentificationTarget(libraryInformation, createComparisonResult(hsp, search));
 				identificationTarget.setIdentifier(report.getVersion());
