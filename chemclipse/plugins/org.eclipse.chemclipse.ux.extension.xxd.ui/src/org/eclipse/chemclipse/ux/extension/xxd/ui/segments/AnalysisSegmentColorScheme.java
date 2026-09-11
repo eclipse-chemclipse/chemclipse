a88e93b4fd74ed6a25798639a6ff115243982f67
Philip Wenig <philip.wenig@openchrom.net>
2026-09-11 22:01:30 +0200

    Update the analysis segment paint listener on settings change now.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/segments/AnalysisSegmentColorScheme.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/segments/AnalysisSegmentColorScheme.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  * Christoph Läubrich - initial API and implementation
+ * Philip Wenig - correct typos
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.xxd.ui.segments;
 
@@ -18,21 +19,20 @@ import org.eclipse.swt.graphics.RGB;
 public class AnalysisSegmentColorScheme {
 
 	public enum Type {
-		SEGMENT_EVEN, SEGMENT_ODD, LINE, SELECTION;
+		SEGMENT_EVEN, //
+		SEGMENT_ODD, //
+		LINE, SELECTION; //
 	}
 
 	/**
-	 * neutral brown
+	 * CHROMATOGRAM = Neutral Brown
+	 * ANALYSIS = Green Scheme
+	 * NOISE = Pink Scheme
 	 */
 	public static final AnalysisSegmentColorScheme CHROMATOGRAM = new AnalysisSegmentColorScheme(new RGB(200, 190, 183), new RGB(227, 222, 219), new RGB(172, 157, 147), new RGB(145, 124, 111));
-	/**
-	 * green sceme
-	 */
 	public static final AnalysisSegmentColorScheme ANALYSIS = new AnalysisSegmentColorScheme(new RGB(215, 244, 227), new RGB(175, 233, 198), new RGB(135, 222, 170), new RGB(95, 211, 141));
-	/**
-	 * pink sceme
-	 */
 	public static final AnalysisSegmentColorScheme NOISE = new AnalysisSegmentColorScheme(new RGB(246, 213, 255), new RGB(238, 170, 255), new RGB(229, 128, 255), new RGB(170, 0, 212));
+
 	private final RGB[] rgb;
 
 	private AnalysisSegmentColorScheme(RGB... colours) {
--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/segments/AnalysisSegmentPaintListener.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/segments/AnalysisSegmentPaintListener.java
@@ -44,6 +44,8 @@ public class AnalysisSegmentPaintListener<X extends IAnalysisSegment> implements
 	private boolean paintLines = false;
 	private int alpha = ALPHA;
 
+	private boolean isDraw = false;
+
 	public AnalysisSegmentPaintListener(AnalysisSegmentColorScheme colorScheme, Supplier<Collection<X>> segmentSupplier, Predicate<X> selectionCheck) {
 
 		this.colorScheme = colorScheme;
@@ -51,52 +53,59 @@ public class AnalysisSegmentPaintListener<X extends IAnalysisSegment> implements
 		this.selectionCheck = selectionCheck;
 	}
 
-	@Override
-	public void paintControl(PaintEvent evt) {
+	public void setDraw(boolean isDraw) {
 
-		Widget widget = evt.widget;
-		if(widget instanceof IPlotArea plotArea) {
-			Chart chart = plotArea.getChart();
-			ISeries<?> series = chart.getSeriesSet().getSeries(ExtendedChromatogramUI.SERIES_ID_CHROMATOGRAM);
-			if(series != null) {
-				GC gc = evt.gc;
-				AnalysisSegmentColors colors = colorScheme.create();
-				IAxis xAxis = chart.getAxisSet().getXAxis(series.getXAxisId());
-				Rectangle clientArea = chart.getClientArea();
-				int y = clientArea.y;
-				int height = clientArea.height;
-				gc.setLineStyle(SWT.LINE_SOLID);
-				boolean alternate = false;
-				gc.setForeground(colors.get(Type.LINE));
-				for(X segment : segmentSupplier.get()) {
-					int x1 = xAxis.getPixelCoordinate(segment.getStartRetentionTime());
-					int x2 = xAxis.getPixelCoordinate(segment.getStopRetentionTime());
-					boolean isSelected = selectionCheck.test(segment);
-					if(isSelected) {
-						gc.setBackground(colors.get(Type.SELECTION));
-						gc.setAlpha(alpha + 50);
-					} else if(alternate) {
-						gc.setBackground(colors.get(Type.SEGMENT_ODD));
-						gc.setAlpha(alpha);
-					} else {
-						gc.setBackground(colors.get(Type.SEGMENT_EVEN));
-						gc.setAlpha(alpha);
-					}
-					if(paintArea || isSelected) {
-						gc.fillRectangle(x1, y, x2 - x1, height);
-					}
-					if(isSelected) {
-						gc.setAlpha(255);
-					} else {
-						gc.setAlpha(alpha + 50);
-					}
-					if(paintLines || isSelected) {
-						if(isSelected && paintArea) {
-							gc.drawLine(x1, 0, x1, height);
+		this.isDraw = isDraw;
+	}
+
+	@Override
+	public void paintControl(PaintEvent paintEvent) {
+
+		if(isDraw) {
+			Widget widget = paintEvent.widget;
+			if(widget instanceof IPlotArea plotArea) {
+				Chart chart = plotArea.getChart();
+				ISeries<?> series = chart.getSeriesSet().getSeries(ExtendedChromatogramUI.SERIES_ID_CHROMATOGRAM);
+				if(series != null) {
+					GC gc = paintEvent.gc;
+					AnalysisSegmentColors colors = colorScheme.create();
+					IAxis xAxis = chart.getAxisSet().getXAxis(series.getXAxisId());
+					Rectangle clientArea = chart.getClientArea();
+					int y = clientArea.y;
+					int height = clientArea.height;
+					gc.setLineStyle(SWT.LINE_SOLID);
+					boolean alternate = false;
+					gc.setForeground(colors.get(Type.LINE));
+					for(X segment : segmentSupplier.get()) {
+						int x1 = xAxis.getPixelCoordinate(segment.getStartRetentionTime());
+						int x2 = xAxis.getPixelCoordinate(segment.getStopRetentionTime());
+						boolean isSelected = selectionCheck.test(segment);
+						if(isSelected) {
+							gc.setBackground(colors.get(Type.SELECTION));
+							gc.setAlpha(alpha + 50);
+						} else if(alternate) {
+							gc.setBackground(colors.get(Type.SEGMENT_ODD));
+							gc.setAlpha(alpha);
+						} else {
+							gc.setBackground(colors.get(Type.SEGMENT_EVEN));
+							gc.setAlpha(alpha);
 						}
-						gc.drawLine(x2, 0, x2, height);
+						if(paintArea || isSelected) {
+							gc.fillRectangle(x1, y, x2 - x1, height);
+						}
+						if(isSelected) {
+							gc.setAlpha(255);
+						} else {
+							gc.setAlpha(alpha + 50);
+						}
+						if(paintLines || isSelected) {
+							if(isSelected && paintArea) {
+								gc.drawLine(x1, 0, x1, height);
+							}
+							gc.drawLine(x2, 0, x2, height);
+						}
+						alternate = !alternate;
 					}
-					alternate = !alternate;
 				}
 			}
 		}
